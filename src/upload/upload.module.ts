f065c63f18d74bb945074b8ebcb28d64f45cabc0
Vlad <koooornienko@gmail.com>
2026-09-20 10:51:43 +0500

    fix(upload): import UserModule in UploadModule to resolve JwtAuthGuard dependency

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, UserModule],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
